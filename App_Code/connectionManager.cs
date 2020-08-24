01adeb1182d5110a21902b19429e35e7075a57aa
Ron-Web-Dotcom <70042056+Ron-Web-Dotcom@users.noreply.github.com>
2020-08-24 02:00:32 -0500

    Add files via upload

---
+++ App_Code/connectionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Security;
+
+/// <summary>
+/// Summary description for connectionManager
+/// </summary>
+public class connectionManager
+{
+    public static SqlConnection GetMembersConnection()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["SignUpDBConnectionString1"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connectionString);
+        connection.Open();
+        return connection;
+    }
+}
